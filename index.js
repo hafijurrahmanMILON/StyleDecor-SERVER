fffd01b73c9b521371f14273d68a361db91a5767
hafijurrahmanMILON <hafijurrahman7085@gmail.com>
2025-12-07 13:07:38 +0600

    server initialized and connected to mongoDB

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@devcluster.k7riodd.mongodb.net/?appName=DevCluster`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const styleDecorDB = client.db("styleDecorDB");
+    const serviceCollection = styleDecorDB.collection("services");
+
+
+   
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("server running fine");
+});
+app.listen(port, () => {
+  console.log("port:", port);
+});
